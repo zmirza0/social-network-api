855358ad9167939819ade5c621e919a0e449bd85
Z. R <zmirza7701@gmail.com>
2023-03-09 16:23:17 -0600

    added models

---
+++ models/Users.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const UsersSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [{}],
+    friends: [{}],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+UsersSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const Users = model("Users", UsersSchema);
+
+module.exports = Users;
